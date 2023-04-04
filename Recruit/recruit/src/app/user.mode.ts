54fd6b612b8fc0852f6e812bb7aeba0f405f64a2
swaroop lenka <78135111+swarooplenka13@users.noreply.github.com>
2023-04-04 18:37:34 +0530

    Delete Recruit directory

--- Recruit/recruit/src/app/user.mode.ts
+++
@@ -1,6 +0,0 @@
-export class User {
-    fullName: string | undefined;
-    email!: string; 
-    password!: string;
-    cpassword!:string;
-}
--- Recruit/seanalysis/ntl.py
+++
@@ -1,5 +0,0 @@
-# import the nltk module
-import nltk
-
-# write this command
-nltk.download()
